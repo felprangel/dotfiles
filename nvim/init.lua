9f0e3eb2a2084f2a6186dcdafde51a8ffc100bfc
felprangel <feliperangel897@gmail.com>
2025-02-06 22:07:07 -0300

    adiciona configs iniciais do neovim

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require("vim-opts")
+require("keymaps")
+require("config.lazy")
+
+vim.cmd.colorscheme "catppuccin"
---
+++ nvim/lua/vim-opts.lua
@@ -0,0 +1,13 @@
+-- Define a tecla lider
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Habilita Numeros e Numeros relativos
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Habilita mouse
+vim.opt.mouse = 'a'
+
+-- Sincroniza o clipboard do OS com o do Vim
+vim.opt.clipboard = 'unnamedplus'
